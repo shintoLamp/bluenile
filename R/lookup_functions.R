b0a9497a8a754b2e82450fa5d63f5a7bbc0a80f7
shintoLamp <bateman.mcbride@phs.scot>
2022-05-24 17:39:25 +0100

    Simplified lookup

--- R/lookup_functions.R
+++ R/lookup_functions.R
@@ -185,8 +185,7 @@ twodigitla_to_hb <- function(df, lcacodename) {
 #'
 #' @examples
 lcaname_to_hb <- function(df, lcaname, region = FALSE) {
-  if (region == FALSE) {
-    df %>% dplyr::mutate(
+    return_df <- df %>% dplyr::mutate(
       hb = dplyr::case_when(
         stringr::str_detect({{ lcaname }}, "(?i)Ayrshire") ~ "NHS Ayrshire & Arran",
         stringr::str_detect({{ lcaname }}, "(?i)Borders") ~ "NHS Borders",
@@ -206,31 +205,6 @@ lcaname_to_hb <- function(df, lcaname, region = FALSE) {
         TRUE ~ "Other Non Scottish Residents"
       )
     )
-  } else {
-    df %>%
-      dplyr::mutate(
-        hb = dplyr::case_when(
-          stringr::str_detect({{ lcaname }}, "(?i)Ayrshire") ~ "NHS Ayrshire & Arran",
-          stringr::str_detect({{ lcaname }}, "(?i)Borders") ~ "NHS Borders",
-          stringr::str_detect({{ lcaname }}, "(?i)Dumfries") ~ "NHS Dumfries & Galloway",
-          stringr::str_detect({{ lcaname }}, "(?i)Fife") ~ "NHS Fife",
-          stringr::str_detect({{ lcaname }}, "(?i)Clack") | stringr::str_detect({{ lcaname }}, "(?i)Falk") | stringr::str_detect({{ lcaname }}, "(?i)Stir") ~ "NHS Forth Valley",
-          stringr::str_detect({{ lcaname }}, "(?i)Aber") | stringr::str_detect({{ lcaname }}, "(?i)Moray") | stringr::str_detect({{ lcaname }}, "(?i)Gramp") ~ "NHS Grampian",
-          stringr::str_detect({{ lcaname }}, "(?i)Dunbar") | stringr::str_detect({{ lcaname }}, "(?i)Renfrew") | stringr::str_detect({{ lcaname }}, "(?i)Glasgow") |
-            stringr::str_detect({{ lcaname }}, "(?i)Inver") ~ "NHS Greater Glasgow & Clyde",
-          stringr::str_detect({{ lcaname }}, "(?i)Argyll") | stringr::str_detect({{ lcaname }}, "(?i)Highland") ~ "NHS Highland",
-          stringr::str_detect({{ lcaname }}, "(?i)Lanark") ~ "NHS Lanarkshire",
-          stringr::str_detect({{ lcaname }}, "(?i)Edinburgh") | stringr::str_detect({{ lcaname }}, "(?i)Lothian") ~ "NHS Lothian",
-          stringr::str_detect({{ lcaname }}, "(?i)Ork") ~ "NHS Orkney",
-          stringr::str_detect({{ lcaname }}, "(?i)Shet") ~ "NHS Shetland",
-          stringr::str_detect({{ lcaname }}, "(?i)Ang") | stringr::str_detect({{ lcaname }}, "(?i)Dund") | stringr::str_detect({{ lcaname }}, "(?i)Perth") ~ "NHS Tayside",
-          stringr::str_detect({{ lcaname }}, "(?i)Western") | stringr::str_detect({{ lcaname }}, "(?i)Siar") ~ "NHS Western Isles",
-          TRUE ~ "Other Non Scottish Residents"
-        )
-      ) %>%
-      dplyr::mutate(hb = dplyr::if_else(hb != "Other Non Scottish Residents", base::paste0(stringr::str_sub(hb, 4, -1), " Region"), hb))
-  }
-}
-
-
-
+  if (region == TRUE) {
+      return_df <- return_df %>% dplyr::mutate(hb = dplyr::if_else(hb != "Other Non Scottish Residents", base::paste0(stringr::str_sub(hb, 4, -1), " Region"), hb))
+  }}

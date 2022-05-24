dd0762343aee18448b908af0316e0b70edca01d1
shintoLamp <bateman.mcbride@phs.scot>
2022-05-24 16:05:51 +0100

    Adding data.R, and ep_file_vars.rda

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Episode file variables
+#'
+#' The names of the variables in the episode files,
+#' in the correct order.
+#'
+#' @name ep_file_vars
+#' @docType data
+#' @keywords data
+"ep_file_vars"

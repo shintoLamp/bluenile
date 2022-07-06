953797fa3e7bafdcddefb95f0409e7345aec4a48
shintoLamp <bateman.mcbride@phs.scot>
2022-07-06 15:40:46 +0100

    Removed locality_lookup() from lookup_functions.R and made a new function, get_locality_lookup()

---
+++ R/get_locality_lookup.R
@@ -0,0 +1,35 @@
+#' Read in most recent Geography team locality lookup file
+#'
+#' @description Read in the most recent locality-level lookup file from the Geographies team with specified columns
+#'
+#'
+#' @param suffix A string in "YYYYMMDD" format correlating to most recent geography file
+#' @param columns A character vector of columns
+#' @param aggregate Should the lookup be aggregated? (default FALSE)
+#' @param matching_variable The level to which the lookup should be aggregated
+#' @param new_variables The variable(s) you wish to match on to your dataset
+#'
+#' @return A tibble with the selected columns to use as a lookup
+#' @export
+#'
+#' @examples
+#' x <- locality_lookup("20200825", c("datazone2011", "hscp_locality"))
+#' y <- x <- get_locality_lookup("20200825", c("ca2019name", "ca2011"), aggregate = TRUE, ca2019name, ca2011)
+#'
+#' @family Lookups
+get_locality_lookup <- function(suffix,
+                                columns,
+                                aggregate = FALSE,
+                                matching_variable,
+                                new_variables) {
+  return <- readr::read_rds(glue::glue("/conf/linkage/output/lookups/Unicode/Geography/HSCP Locality/HSCP Localities_DZ11_Lookup_{suffix}.rds")) %>%
+    dplyr::select(dplyr::all_of(columns))
+  if (aggregate == TRUE) {
+    return <- return %>%
+      dplyr::group_by({{ matching_variable }}) %>%
+      dplyr::summarise(dplyr::across({{ new_variables }}, dplyr::first))
+    return(return)
+  } else {
+    return(return)
+  }
+}
--- R/lookup_functions.R
+++ R/lookup_functions.R
@@ -1,25 +1,3 @@
-#' Read in most recent Geography team locality lookup file
-#'
-#' @param suffix A string in "YYYYMMDD" format correlating to most recent geography file
-#' @param columns A character vector of columns
-#'
-#' @return A tibble with the selected columns to use as a lookup
-#' @export
-#'
-#' @examples
-#' x <- locality_lookup("20200825", c("datazone2011", "hscp_locality"))
-#'
-#' @importFrom readr read_rds
-#' @importFrom magrittr %>%
-#' @importFrom janitor clean_names
-#' @importFrom dplyr select all_of
-#' @importFrom glue glue
-locality_lookup <- function(suffix, columns) {
-  read_rds(glue("/conf/linkage/output/lookups/Unicode/Geography/HSCP Locality/HSCP Localities_DZ11_Lookup_{suffix}.rds")) %>%
-    clean_names() %>%
-    select(all_of(columns))
-}
-
 #' Read in a lookup file for Specialty codes
 #'
 #' @param columns A vector of columns to pass to dplyr::select.
